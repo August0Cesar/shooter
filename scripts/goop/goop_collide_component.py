05a8e6ff3b0dde812a96272d221720cde2b87c46
Augusto Matos <augusto.matos@luizalabs.com>
2021-08-19 09:18:42 -0300

    feat: adicionado personagem para player

--- scripts/goop/goop_collide_component.py
+++ scripts/goop/goop_collide_component.py
@@ -6,12 +6,14 @@ class GoopCollideComponent(bge.types.KX_PythonComponent):
 
     def start(self, args):
         self.object.collisionCallbacks.append(self.on_collison)
+        self.objects_collide = self.object["objects_collide"].split(",")
         self.colidiu = False
     
     def update(self):
         ...
     
     def on_collison(self, obj):
-        if not self.colidiu:
-            self.colidiu = True
-            obj['vida'] -= 20
+        if obj.name in self.objects_collide:
+            if not self.colidiu:
+                self.colidiu = True
+                obj['vida'] -= 20
